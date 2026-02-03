ca72e9b9b26c2e9b64bcc2757ff7a35db027c9aa
A-PachecoT <andre.pacheco.t@uni.pe>
2026-02-02 19:09:32 -0500

    feat: portfolio cinematográfico Jonathan Navarro

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://cofoundy.github.io",
+  base: "/portfolio-jonathan-navarro",
   vite: {
     plugins: [tailwindcss()],
   },
